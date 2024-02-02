85b5ccb283f8d4985c829bfaf3f170f7292a1c9f
Timur <Timur.polenov@gmail.com>
2024-02-02 22:36:39 +0300

    added rep and ser+ config

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"log"
-	"github.com/timut2/project2/pkg/handler"
+	"github.com/timut2/Practice3/pkg/handler"
+	"github.com/timut2/Practice3"
+	"github.com/spf13/viper"
+	"github.com/timu2/Practice3/repository"
+	"github.com/timu2/Practice3/service"
 )
 func main(){
-	handlers:= new(handler.Handler)
-
+	if err:= initConfig(); err!= nil{
+		log.Fatalf("error init config",err)
+	}
+	repos:= repository.NewRepository()
+	services:=service.NewService(repos)
+	handlers:= new(services)
 	s:= new(server.Server)
-	if err:= s.Run("8080", handlers.InitRoutes()); err!= nil{
+	if err:= s.Run(viper.GetString("port"), handlers.InitRoutes()); err!= nil{
 		log.Fatalf("error occured ",err)
 	}
+}
+
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadConfig()
 }
